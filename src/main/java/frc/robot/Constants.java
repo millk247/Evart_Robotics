40186888587ba27027c98dbf8c07d0452f959686
millk247 <122893001+millk247@users.noreply.github.com>
2023-02-25 13:44:22 -0500

    Worked on ID more motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,9 @@ package frc.robot;
 
 public class Constants {
     
-public static int Victor_1_ID  = 1;   
-public static int Victor_2_ID  = 2;
-public static int Victor_11_ID = 11;
-public static int Victor_12_ID = 12;
+public static int Victor_1_ID  = 1;   // left drive 1
+public static int Victor_2_ID  = 2; // left drive 2
+public static int Victor_11_ID = 11; // right drive 1
+public static int Victor_12_ID = 12; // right drive 2
     
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,41 +4,55 @@
 
 package frc.robot;
 
-
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the manifest
+ * file in the resource
  * directory.
  */
 public class Robot extends TimedRobot {
+
   private final WPI_VictorSPX m_leftDrive1 = new WPI_VictorSPX(1);
   private final WPI_VictorSPX m_leftDrive2 = new WPI_VictorSPX(2);
-  // private final WPI_VictorSPX m_arm = new WPI_VictorSPX(3);
-  
+
+  // private final WPI_VictorSPX m_arm = new WPI_VictorSPX(3); // Moves arm
+
   private final WPI_VictorSPX m_rightDrive1 = new WPI_VictorSPX(11);
   private final WPI_VictorSPX m_rightDrive2 = new WPI_VictorSPX(12);
+  private final WPI_VictorSPX some_motor = new WPI_VictorSPX(10); // customize to the controller
+
+  // Spark MAX can be used in CAN bus or PWM. If using CAN, set CAN ID in the REV Client
+  private final PWMSparkMax vSlide = new PWMSparkMax(14); // change number where ever its plugged  
 
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive1, m_rightDrive1);
-  private final Joystick m_Joystick = new Joystick(0);
+  private final Joystick m_driver = new Joystick(0);
+  private final Joystick operator = new Joystick(1);
+
   // private final XboxController m_controller = new XboxController(0);
-  // private final XboxController m_controller = new XboxController(0); // Second controller goes here
+  // private final XboxController m_controller = new XboxController(0); // Second
+  // controller goes here
   private final Timer m_timer = new Timer();
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
@@ -52,7 +66,7 @@ public class Robot extends TimedRobot {
     m_leftDrive2.set(ControlMode.Follower, Constants.Victor_1_ID);
     m_rightDrive2.set(ControlMode.Follower, Constants.Victor_11_ID);
 
-    //2 is follower of 1, and 12 is follwoer of 11
+    // 2 is follower of 1, and 12 is follwoer of 11
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
@@ -74,25 +88,59 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
+  /**
+   * This function is called once each time the robot enters teleoperated mode.
+   */
   @Override
-  public void teleopInit() {}
-
- 
+  public void teleopInit() {
+  }
 
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_Joystick.getY(), -m_Joystick.getX());
+    m_robotDrive.arcadeDrive(-m_driver.getY(), -m_driver.getX());
     // m_robotDrive.arcadeDrive(-m_controller.getLeftY(), -m_controller.getLeftX());
     // m_arm.set(ControlMode.PercentOutput, m_controller.getRightY());
-  }
+
+    // move some motor
+    // Each button has numeric value that needs to be looked up online
+    if (m_driver.getRawButtonPressed(1)) {
+      some_motor.set(.5); // turn on some motor at half speed (0.5) set a value between 0 and 1. 
+      // Make set value negative to run motor other direction
+    } else {
+      some_motor.set(0.0);
+    } // turn off a motor
+
+
+    if (operator.getRawButtonPressed(1)) {
+      vSlide.set(0.2); // turn on some motor at set a value between 0 and 1. 
+      // Make set value negative to run motor other direction
+    } else {
+      vSlide.set(0.0);
+    } // turn off a motor
+
+    if (operator.getRawButtonPressed(2)) {
+      vSlide.set(-0.2); // turn on some motor at set a value between 0 and 1. 
+      // Make set value negative to run motor other direction
+    } else {
+      vSlide.set(0.0);
+    } // turn off a motor
+
+  } // end of teleop period
 
   /** This function is called once each time the robot enters test mode. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
+
+/*
+ * Vendor Library for REV
+ * https://software-metadata.revrobotics.com/REVLib-2023.json
+ * 
+ */
