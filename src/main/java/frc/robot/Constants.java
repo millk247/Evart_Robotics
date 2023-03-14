a69e7a754d42d0fb89c150563081c76cf01cc347
millk247 <122893001+millk247@users.noreply.github.com>
2023-03-14 02:59:52 -0400

    Xbox and solenoid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,5 +9,29 @@ public static int Victor_12_ID = 12; // right drive 2
 public static int Victor_21_ID = 21; // vertical motor
 public static int Victor_22_ID = 22; // horizontial motor
 public static int Spark_23_ID = 23; // rotational motor
-    
+
+//pneumatics
+  //private static final int kSolenoidButton = 3;
+public static final int kDoubleSolenoidForward = 4;
+public static final int kDoubleSolenoidReverse = 1;
+
+//XboxOne Joysticks
+public static int LEFT_STICK_X = 0;
+public static int LEFT_STICK_Y = 1;
+public static int LEFT_TRIGGER = 2;
+public static int RIGHT_TRIGGER = 3;
+public static int RIGHT_STICK_X = 4;
+public static int RIGHT_STICK_Y = 5;
+
+//XboxOne Buttons
+public static int A = 1;
+public static int B = 2;
+public static int X = 3;
+public static int Y = 4;
+public static int LB = 5;
+public static int RB = 6;
+public static int LOGO_LEFT = 7;
+public static int LOGO_RIGHT = 8;
+public static int LEFT_STICK_BUTTON = 9;
+public static int RIGHT_STICK_BUTTON = 10;
 }
